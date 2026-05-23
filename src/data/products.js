2ff6c6db052e8f0a8bffacecd5461665c1f86037
ilham el guerje <elgourjeilham@gmail.com>
2026-05-23 21:19:35 +0100

    Add new product images and update products

--- src/data/products.js
+++ src/data/products.js
@@ -1,6 +1,6 @@
 import filterImg from "../assets/images/filter.jpg";
 import freinage from "../assets/images/Kit_freinage.jpg";
-import Huile from "../assets/images/vasco.jpg";
+import Huile from "../assets/images/huile.jpg";
 import Amortisseur from "../assets/images/Amortisseur.jpg";
 import dentImg from "../assets/images/images.jpg";
 import pompeHydraulique from "../assets/images/pompeHydraulique.png";
@@ -18,8 +18,126 @@ import verinsImg from "../assets/images/verinsImg.png";
 import commandePneumatiqueImg from "../assets/images/commandePneumatiqueImg.png";
 import flexibleHydraulique from "../assets/images/flexibleHydraulique.png";
 
+import filter1 from "../assets/images/filters/filter1.jpg";
+import filter2 from "../assets/images/filters/filter2.jpg";
+import filter3 from "../assets/images/filters/filter3.jpg";
+import filter4 from "../assets/images/filters/filter4.jpg";
+import filter5 from "../assets/images/filters/filter5.jpg";
+import filter6 from "../assets/images/filters/filter6.jpg";
+import filter7 from "../assets/images/filters/filter7.jpg";
+import filter8 from "../assets/images/filters/filter8.jpg";
 
+import freinage1 from "../assets/images/brake-kit/freinage1.jpg";
+import freinage2 from "../assets/images/brake-kit/freinage2.jpg";
+import freinage3 from "../assets/images/brake-kit/freinage3.jpg";
+import freinage4 from "../assets/images/brake-kit/freinage4.jpg";
+import freinage5 from "../assets/images/brake-kit/freinage5.jpg";
+import freinage6 from "../assets/images/brake-kit/freinage6.jpg";
+import freinage7 from "../assets/images/brake-kit/freinage7.jpg";
+import freinage8 from "../assets/images/brake-kit/freinage8.jpg";
 
+import huile1 from "../assets/images/huile/huile1.png";
+import huile2 from "../assets/images/huile/huile2.jpg";
+
+import Amortisseur1 from "../assets/images/Amortisseur/Amortisseur1.png";
+import Amortisseur2 from "../assets/images/Amortisseur/Amortisseur2.png";
+import Amortisseur3 from "../assets/images/Amortisseur/Amortisseur3.jpg";
+import Amortisseur4 from "../assets/images/Amortisseur/Amortisseur4.jpg";
+import Amortisseur5 from "../assets/images/Amortisseur/Amortisseur5.jpg";
+import Amortisseur6 from "../assets/images/Amortisseur/Amortisseur6.jpg";
+
+import Dent1 from "../assets/images/Dent/Dent1.jpg";
+import Dent2 from "../assets/images/Dent/Dent2.jpg";
+import Dent3 from "../assets/images/Dent/Dent3.jpg";
+import Dent4 from "../assets/images/Dent/Dent4.jpg";
+import Dent5 from "../assets/images/Dent/Dent5.jpg";
+import Dent6 from "../assets/images/Dent/Dent6.jpg";
+
+import Pompe1 from "../assets/images/Pompe/Pompe1.jpg";
+import Pompe2 from "../assets/images/Pompe/Pompe2.jpg";
+import Pompe3 from "../assets/images/Pompe/Pompe3.jpg";
+import Pompe4 from "../assets/images/Pompe/Pompe4.jpg";
+import Pompe5 from "../assets/images/Pompe/Pompe5.jpg";
+import Pompe6 from "../assets/images/Pompe/Pompe6.jpg";
+import Pompe7 from "../assets/images/Pompe/Pompe7.jpg";
+
+import Capteur1 from "../assets/images/Capteur/Capteur1.jpg";
+import Capteur2 from "../assets/images/Capteur/Capteur2.jpg";
+import Capteur3 from "../assets/images/Capteur/Capteur3.jpg";
+import Capteur4 from "../assets/images/Capteur/Capteur4.jpg";
+import Capteur5 from "../assets/images/Capteur/Capteur5.jpg";
+import Capteur6 from "../assets/images/Capteur/Capteur6.jpg";
+
+import Batterie1 from "../assets/images/Batterie/Batterie1.jpg";
+import Batterie2 from "../assets/images/Batterie/Batterie2.jpg";
+import Batterie3 from "../assets/images/Batterie/Batterie3.jpg";
+import Batterie4 from "../assets/images/Batterie/Batterie4.jpg";
+
+import Godet1 from "../assets/images/Godet/Godet1.jpg";
+import Godet2 from "../assets/images/Godet/Godet2.jpg";
+import Godet3 from "../assets/images/Godet/Godet3.jpg";
+import Godet4 from "../assets/images/Godet/Godet4.jpg";
+import Godet5 from "../assets/images/Godet/Godet5.jpg";
+
+import joint1 from "../assets/images/joint/joint1.jpg";
+import joint2 from "../assets/images/joint/joint2.jpg";
+import joint3 from "../assets/images/joint/joint3.jpg";
+import joint4 from "../assets/images/joint/joint4.jpg";
+import joint5 from "../assets/images/joint/joint5.jpg";
+
+import Courroies1 from "../assets/images/Courroies/Courroies1.jpg";
+import Courroies2 from "../assets/images/Courroies/Courroies2.jpg";
+import Courroies3 from "../assets/images/Courroies/Courroies3.jpg";
+import Courroies4 from "../assets/images/Courroies/Courroies4.jpg";
+import Courroies5 from "../assets/images/Courroies/Courroies5.jpg";
+import Courroies6 from "../assets/images/Courroies/Courroies6.jpg";
+import Courroies7 from "../assets/images/Courroies/Courroies7.jpg";
+
+import Accessoires1 from "../assets/images/Accessoires/Accessoires1.jpg";
+import Accessoires2 from "../assets/images/Accessoires/Accessoires2.jpg";
+import Accessoires3 from "../assets/images/Accessoires/Accessoires3.jpg";
+import Accessoires4 from "../assets/images/Accessoires/Accessoires4.jpg";
+import Accessoires5 from "../assets/images/Accessoires/Accessoires5.jpg";
+import Accessoires6 from "../assets/images/Accessoires/Accessoires6.jpg";
+
+import Distributeur1 from "../assets/images/Distributeur/Distributeur1.jpg";
+import Distributeur2 from "../assets/images/Distributeur/Distributeur2.jpg";
+import Distributeur3 from "../assets/images/Distributeur/Distributeur3.jpg";
+import Distributeur4 from "../assets/images/Distributeur/Distributeur4.jpg";
+import Distributeur5 from "../assets/images/Distributeur/Distributeur5.jpg";
+import Distributeur6 from "../assets/images/Distributeur/Distributeur6.jpg";
+
+import Réservoir1 from "../assets/images/Réservoir/Réservoir1.jpg";
+import Réservoir2 from "../assets/images/Réservoir/Réservoir2.jpg";
+import Réservoir3 from "../assets/images/Réservoir/Réservoir3.jpg";
+import Réservoir4 from "../assets/images/Réservoir/Réservoir4.jpg";
+import Réservoir5 from "../assets/images/Réservoir/Réservoir5.jpg";
+import Réservoir6 from "../assets/images/Réservoir/Réservoir6.jpg";
+import Réservoir7 from "../assets/images/Réservoir/Réservoir7.jpg";
+
+import Vérins1 from "../assets/images/Vérins/Vérins1.jpg";
+import Vérins2 from "../assets/images/Vérins/Vérins2.jpg";
+import Vérins3 from "../assets/images/Vérins/Vérins3.jpg";
+import Vérins5 from "../assets/images/Vérins/Vérins5.jpg";
+import Vérins6 from "../assets/images/Vérins/Vérins6.jpg";
+import Vérins7 from "../assets/images/Vérins/Vérins7.jpg";
+import Vérins8 from "../assets/images/Vérins/Vérins8.jpg";
+
+import pneumatique1 from "../assets/images/pneumatique/pneumatique1.jpg";
+import pneumatique2 from "../assets/images/pneumatique/pneumatique2.jpg";
+import pneumatique3 from "../assets/images/pneumatique/pneumatique3.jpg";
+import pneumatique4 from "../assets/images/pneumatique/pneumatique4.jpg";
+import pneumatique5 from "../assets/images/pneumatique/pneumatique5.jpg";
+import pneumatique6 from "../assets/images/pneumatique/pneumatique6.jpg";
+
+import hydraulique1 from "../assets/images/hydraulique/hydraulique1.jpg";
+import hydraulique2 from "../assets/images/hydraulique/hydraulique2.jpg";
+import hydraulique3 from "../assets/images/hydraulique/hydraulique3.jpg";
+import hydraulique4 from "../assets/images/hydraulique/hydraulique4.jpg";
+import hydraulique5 from "../assets/images/hydraulique/hydraulique5.jpg";
+import hydraulique6 from "../assets/images/hydraulique/hydraulique6.jpg";
+import hydraulique7 from "../assets/images/hydraulique/hydraulique7.jpg";
+import hydraulique8 from "../assets/images/hydraulique/hydraulique8.jpg";
 export const products = [
   {
     id: 1,
@@ -28,7 +146,9 @@ export const products = [
     brand: "Volvo",
     status: "Disponible",
     image:filterImg,
-    description: "Compatible avec la plupart des moteurs diesel lourds.",
+    description: "Filtre à huile premium offrant une protection maximale du moteur et une excellente capacité de filtration. Idéal pour les camions et véhicules industriels nécessitant performance et fiabilité.",
+    images: [filter1, filter2, filter3, filter4,filter5,filter6,filter7,filter8],
+
   },
   {
     id: 2,
@@ -37,7 +157,8 @@ export const products = [
     brand: "Scania",
     status: "Disponible",
     image:freinage,
-    description: "Pour camions Volvo et Scania.",
+    description: "Système de freinage premium offrant une excellente puissance de freinage et une résistance optimale aux hautes températures. Conçu pour assurer sécurité, performance et longévité sur les véhicules lourds..",
+    images: [freinage1, freinage2, freinage3, freinage4, freinage5, freinage6, freinage7, freinage8],
   },
   {
     id: 3,
@@ -46,7 +167,8 @@ export const products = [
     brand: "MAN",
     status: "Disponible",
     image:Huile,
-    description: "Qualité professionnelle pour usage intensif.",
+    description: "Équipement professionnel offrant robustesse et performance optimale.Fabriqué avec des matériaux de haute qualité pour garantir une longue durée de vie et une utilisation intensive..",
+    images:[huile1,huile2,]
   },
   {
     id: 4,
@@ -55,7 +177,8 @@ export const products = [
     brand: "Volvo",
     status: "Disponible",
     image: Amortisseur,
-    description: "Compatible avec tracteurs et bulldozers.",
+    description: "Système de suspension haute performance garantissant une excellente absorption des vibrations et une conduite plus stable. Idéal pour les camions, tracteurs et engins industriels exigeants..",
+    images: [Amortisseur1, Amortisseur2, Amortisseur3, Amortisseur4, Amortisseur5, Amortisseur6],
   },
   
  {
@@ -66,6 +189,7 @@ export const products = [
     status: "Disponible",
     image: dentImg,
     description: "Dents et adaptateurs robustes pour engins de chantier (tracteurs, pelles mécaniques). Haute résistance pour usage intensif.",
+    images :[Dent1, Dent2, Dent3, Dent4, Dent5, Dent6]
   },
   {
   id: 7,
@@ -76,6 +200,7 @@ export const products = [
   ref: "PMP-007-HD",
   image: pompeHydraulique,
   description: "Pompe hydraulique robuste pour engins de chantier CAT et machines lourdes.",
+  images :[Pompe1, Pompe2, Pompe3, Pompe4, Pompe5, Pompe6, Pompe7]
 },
 {
   id: 8,
@@ -86,6 +211,7 @@ export const products = [
   ref: "CPT-008-HD",
   image: capteur,
   description: "Capteur hydraulique haute précision pour engins de chantier et systèmes industriels lourds.",
+  images :[Capteur1, Capteur2, Capteur3, Capteur4, Capteur5, Capteur6],
 },
 {
   id: 9,
@@ -94,8 +220,9 @@ export const products = [
   brand: "CAT",
   status: "Disponible",
   ref: "BAT-009-HD",
-  image: battery,
+  image: Batterie1,
   description: "Batterie robuste et fiable pour engins de chantier, camions lourds et équipements industriels.",
+ images :[Batterie1,Batterie2,Batterie3,Batterie4],
 },
 {
   id: 10,
@@ -106,6 +233,8 @@ export const products = [
   ref: "GDT-010-HD",
   image: Godet,
   description: "Godet robuste et renforcé pour excavatrices et engins de chantier lourds, conçu pour une résistance maximale.",
+  images :[Godet1,Godet2,Godet3,Godet4,Godet5]
+    
 },
 {
   id: 11,
@@ -114,8 +243,9 @@ export const products = [
   brand: "MAN",
   status: "Disponible",
   ref: "JTR-011-HD",
-  image: Joint_Torique,
+  image: joint1,
   description: "Joint torique haute résistance pour systèmes hydrauliques et équipements industriels lourds.",
+  images :[joint1, joint2, joint3, joint4, joint5],
 },
 {
   id: 12,
@@ -124,8 +254,9 @@ export const products = [
   brand: "Volvo",
   status: "Disponible",
   ref: "CRR-012-HD",
-  image: Courroies,
+  image: Courroies1,
   description: "Courroies haute résistance pour moteurs industriels, engins lourds et systèmes de transmission.",
+  images :[Courroies1, Courroies2, Courroies3, Courroies4, Courroies5, Courroies6,Courroies7],
 },
 {
   id: 13,
@@ -134,8 +265,9 @@ export const products = [
   brand: "Mercedes-Benz",
   status: "Disponible",
   ref: "ACC-013-HD",
-  image: Accessoires,
+  image: Accessoires1,
   description: "Ensemble d’accessoires et équipements pour engins lourds et véhicules industriels.",
+  images :[Accessoires1,Accessoires2,Accessoires3,Accessoires4,Accessoires5,Accessoires6]
 },
 {
   id: 14,
@@ -154,8 +286,9 @@ export const products = [
   brand: "Volvo",
   status: "Disponible",
   ref: "DST-015-HD",
-  image: distributeur,
+  image: Distributeur1,
   description: "Distributeur hydraulique haute précision pour systèmes industriels et engins de chantier lourds.",
+  images :[Distributeur1,Distributeur2,Distributeur3,Distributeur4,Distributeur5,Distributeur6],
 },
 {
   id: 16,
@@ -164,8 +297,9 @@ export const products = [
   brand: "DAF",
   status: "Disponible",
   ref: "RSV-016-HD",
-  image: Reservoir,
+  image: Réservoir1,
   description: "Réservoir hydraulique robuste pour systèmes industriels et engins de chantier lourds.",
+  images:[Réservoir1,Réservoir2,Réservoir3,Réservoir4,Réservoir5,Réservoir6,Réservoir7],
 },
 {
   id: 18,
@@ -176,6 +310,7 @@ export const products = [
   ref: "VRN-018-HD",
   image: verinsImg,
   description: "Vérins hydrauliques robustes pour engins de chantier et équipements industriels lourds.",
+  images :[Vérins1,Vérins2,Vérins3,Vérins5,Vérins6,Vérins7,Vérins8],
 },
 {
   id: 19,
@@ -186,6 +321,7 @@ export const products = [
   ref: "CMD-019-HD",
   image: commandePneumatiqueImg,
   description: "Commande pneumatique haute précision pour systèmes industriels et engins de chantier lourds.",
+  images :[pneumatique1,pneumatique2,pneumatique3,pneumatique4,pneumatique5,pneumatique6]
 },
 {
   id: 20,
@@ -195,8 +331,8 @@ export const products = [
   status: "Disponible",
   ref: "FLX-020-HD",
   image: flexibleHydraulique,
-  description:
-    "Flexible hydraulique haute pression pour camions, engins lourds et systèmes industriels.",
+  description:"Flexible hydraulique haute pression pour camions, engins lourds et systèmes industriels.",
+  images :[hydraulique1,hydraulique2,hydraulique3,hydraulique4,hydraulique5,hydraulique6,hydraulique7,hydraulique8],
 },
   
 
--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { products } from "../../data/products";
 import { FaWhatsapp, FaEnvelope } from "react-icons/fa";
@@ -17,92 +17,83 @@ function ProductDetails() {
     );
   }
 
+  const [mainImage, setMainImage] = useState(product.image);
+
   return (
     <div className="details-page">
       <div className="back-container">
-            <Link className="back-link" to="/produits">
-                ← Retour aux produits
-            </Link>
+        <Link className="back-link" to="/produits">
+          ← Retour aux produits
+        </Link>
       </div>
 
       <div className="details-wrapper">
         <div className="details-top">
           <div className="details-image-card">
-            <img src={product.image} alt={product.name} />
+            <img src={mainImage} alt={product.name} />
+
+            <div className="thumbnails">
+              {(product.images || [product.image]).map((img, index) => (
+                <img
+                  key={index}
+                  src={img}
+                  alt=""
+                  className={`thumb ${mainImage === img ? "active" : ""}`}
+                  onClick={() => setMainImage(img)}
+                />
+              ))}
+            </div>
           </div>
 
           <div className="details-info-card">
             <span className="detail-badge">{product.category}</span>
+
             <h1>{product.name}</h1>
 
-            <p><strong>Référence:</strong> {product.ref || "FLT-001-HD"}</p>
-            <p><strong>Marque:</strong> {product.brand}</p>
+            <div className="product-meta">
+              <p>
+                <strong>Référence:</strong>
+                <span>{product.ref || "FLT-001-HD"}</span>
+              </p>
+
+              <p>
+                <strong>Marque:</strong>
+                <span>{product.brand}</span>
+              </p>
+            </div>
 
             <div className="stock-detail">
-                <span className="stock-icon">✔</span>
-                <span>En stock - Disponible immédiatement</span>
+              <span className="stock-icon">✔</span>
+              <span>En stock - Disponible immédiatement</span>
             </div>
+
             <hr />
 
             <h3>Description</h3>
+
             <p className="description-detail">{product.description}</p>
 
             <div className="buttons-group">
-                <a
-                    className="whatsapp-btn"
-                    href={`https://wa.me/212700808100?text=Bonjour, je veux ${product.name}`}
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <FaWhatsapp className="btn-icon" />
-                    Contacter via WhatsApp
-                </a>
+              <a
+                className="whatsapp-btn"
+                href={`https://wa.me/212700808100?text=Bonjour, je veux ${product.name}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaWhatsapp className="btn-icon" />
+                Contacter via WhatsApp
+              </a>
 
-                <a
-                    className="email-btn"
-                    href={`https://mail.google.com/mail/?view=cm&fs=1&to=c.harika@gmail.com&su=Demande produit&body=Bonjour,%0D%0AJe suis intéressé par: ${product.name}%0D%0AMerci.`}
-                    target="_blank"
-                    rel="noreferrer"
-                    >
-                    <FaEnvelope className="btn-icon" />
-                    Envoyer un email
-                </a>
+              <a
+                className="email-btn"
+                href={`https://mail.google.com/mail/?view=cm&fs=1&to=c.harika@gmail.com&su=Demande produit&body=Bonjour,%0D%0AJe suis intéressé par: ${product.name}%0D%0AMerci.`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaEnvelope className="btn-icon" />
+                Envoyer un email
+              </a>
             </div>
-           
-
-            <div className="mini-info">
-             
-              <div>
-                <span>🛡</span>
-                <strong>Garantie</strong>
-                <p>2 ans</p>
-              </div>
-              <div>
-                <span>↩</span>
-                <strong>Retour</strong>
-                <p>30 jours</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="details-bottom">
-          <div className="spec-card">
-            <h2>Spécifications Techniques</h2>
-            <div className="spec-row"><span>Type</span><strong>{product.category}</strong></div>
-            <div className="spec-row"><span>Compatibilité</span><strong>Moteurs Volvo, DAF, MAN</strong></div>
-            <div className="spec-row"><span>Diamètre</span><strong>82 mm</strong></div>
-            <div className="spec-row"><span>Hauteur</span><strong>142 mm</strong></div>
-            <div className="spec-row"><span>Pression max</span><strong>10 bar</strong></div>
-          </div>
-
-          <div className="features-card">
-            <h2>Caractéristiques</h2>
-            <p>✓ Média filtrant haute efficacité</p>
-            <p>✓ Valve anti-retour intégrée</p>
-            <p>✓ Résistant aux hautes températures</p>
-            <p>✓ Certification ISO 9001</p>
-            <p>✓ Garantie constructeur 2 ans</p>
           </div>
         </div>
       </div>
